e1e6f75aa9e8cbfd2e9dd2ce058cc02b1fcc6462
885685845 <96099327+RoshanIndane@users.noreply.github.com>
2022-08-28 17:48:43 +0530

    complete app

---
+++ client/src/App.jsx
@@ -0,0 +1,30 @@
+
+
+import { useState } from 'react'
+import { Route, Routes } from 'react-router'
+import './App.css'
+import Home from './Components/Home/home'
+import { Login } from './Components/login/login'
+import { Navbar } from './Components/Navbar/navbar'
+import { SignUp } from './Components/SignUp/signup'
+
+function App() {
+ 
+
+ 
+  return (
+    <div className="App">
+     <Navbar></Navbar>
+     <Routes>
+     
+     <Route path='/' element={<Home></Home>}></Route>
+      <Route path='/login' element={<Login></Login>}></Route>
+      <Route path='/signup' element={<SignUp></SignUp>}></Route>
+
+     </Routes>
+    
+    </div>
+  )
+}
+
+export default App
